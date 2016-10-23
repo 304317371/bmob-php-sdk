f1011be56caa7abc58e75b58876020169f44a7b7
Stuart CROSBY <stuart@lesee.tv>
2016-10-23 22:10:35 +0800

    format

--- lib/BmobConfig.class.php
+++ lib/BmobConfig.class.php
@@ -6,14 +6,9 @@
 
  * @license http://www.gnu.org/copyleft/lesser.html Distributed under the Lesser General Public License (LGPL)
  */
-class BmobConfig
-{
+class BmobConfig {
      const APPID = '3ed372368a82076b5762308c4ad607de';  //后台"应用密钥"中的Application ID
      const RESTKEY = '6d1ade386621049fbcafe63ea55dc34e';  //后台"应用密钥"中的REST API Key
      const BMOBURL = 'https://api.bmob.cn/1/';  //请勿修改
      const BMOBURLTWO = 'https://api.bmob.cn/2/';  //请勿修改
-
-
-
-
 }
